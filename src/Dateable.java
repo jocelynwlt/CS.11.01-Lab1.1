e1ae59212c45c6a57eaabcd3d66678710afe503a
jocelyn <jocelynwlt2026@student.cis.edu.hk>
2023-09-08 21:46:27 +0800

    finished

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        System.out.println("Please enter your age: ");
+        Scanner scanner = new Scanner(System.in);
+        int userAge = scanner.nextInt();
+        double halfUserAge = userAge/2.0;
+        double dateableAge = halfUserAge+7.0;
+        System.out.println(userAge + " year olds should date somebody who is at least " + dateableAge + " years old.");
+
+
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        System.out.println("Please enter your age: ");
+        Scanner scanner = new Scanner(System.in);
+        int userAge = scanner.nextInt();
+        int pages = 100-userAge;
+        System.out.println(userAge + " year olds should read at least " + pages + " before giving up on a book.");
+
 
     }
 }
